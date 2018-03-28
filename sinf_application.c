9d0367b2435874dfb0c5d3b44d5e8724a6d1dcec
joaoreis-feup <32265487+joaoreis-feup@users.noreply.github.com>
2018-03-28 11:51:03 +0100

    Add files via upload

--- sinf_application.c
+++ sinf_application.c
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/*
+ * In order to add or remove rooms from this working version, two changes need to be made.
+ * 
+ * 1) Change the definition NUMBER_ROOMS to the desired value, e.g. 2 if you only want to rooms
+ * 2) Change the global variable temp_threshold with the intended threasholds. The number of thresholds
+ * should match the NUMBER_ROOMS, e.g. int temp_threshold[NUMBER_ROOMS] = {8,17};
+ * 
+ * After these changes, you only need to compile, build and execute your code.
+ * 
+ * */
+
 
 #define MAX_NAME_SZ 256
 #define NUMBER_ROOMS 3
@@ -17,9 +28,7 @@ int main()
 	struct Actuators actuators[NUMBER_ROOMS];
 
 	int print_values = 0;
-	
 	char str[MAX_NAME_SZ];
-	
 	
 	while(1){
 		
@@ -45,7 +54,6 @@ int main()
 		}else{
 			writeRGBMatrix(sensors,actuators,NUMBER_ROOMS,temp_threshold,NUMBER_CELLS);	
 		}
-				
 	}
 	
 	return 0;
